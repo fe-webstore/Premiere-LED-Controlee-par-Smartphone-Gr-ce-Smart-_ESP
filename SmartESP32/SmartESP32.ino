c8dbd3695dbea3a3f60b90618feb1a18228fed64
Smart_ESP <aguiadahoyann@gmail.com>
2025-07-10 14:52:07 +0200

    Update SmartESP32.ino

--- SmartESP32/SmartESP32.ino
+++ SmartESP32/SmartESP32.ino
@@ -1,19 +1,27 @@
 #include "SmartESP32Utils.h"
 
-const char* ssid = "N1";
-const char* password = "yannyann";
 
+// ------ Connexion WiFi --------
+const char* ssid = "N4";
+const char* password = "00000000";
+
+// ------ Variables globales ------
 AsyncWebServer server(80);
 String lastCommand = "";
 AutoUpdatePayload payload;
 unsigned long lastUpdateTime = 0;
 const unsigned long updateInterval = 500;
 
+// ------- Déclaration des pins --------
+// Choisis un GPIO dispo pour la LED, par exemple GPIO 23
+int salon = 23;
+
 void setup() {
   Serial.begin(115200);
   connectToWiFi(ssid, password);
   setupWebSocket(server, &lastCommand);
-  pinMode(23, OUTPUT);  // Ex: GPIO23 (ESP32) au lieu de D1
+
+  pinMode(salon, OUTPUT);
 }
 
 void loop() {
@@ -23,25 +31,24 @@ void loop() {
 
 void Smartcommande() {
   if (!lastCommand.isEmpty()) {
-    if (lastCommand == "on") {
-      digitalWrite(23, HIGH);
+    if (lastCommand == "led_on") {
+      digitalWrite(salon, HIGH);
       payload.bulb1 = "true";
-      payload.ia = "OK, j’ai allumé la LED";
-      payload.notif = "LED ON 💡";
-    } else if (lastCommand == "off") {
-      digitalWrite(23, LOW);
+      Serial.println("salon est ok");
+
+    } else if (lastCommand == "led_off") {
+      digitalWrite(salon, LOW);
       payload.bulb1 = "false";
-      payload.ia = "LED éteinte";
-      payload.notif = "LED OFF ❌";
+      Serial.println("salon est off");
+
     } else {
-      payload.ia = "false";
-      payload.notif = "false";
+      Serial.println("notification");
+      payload.notif = "okok la led recoi une commande inconnue";
     }
 
     lastCommand = "";
     sendAutoUpdate(payload);
     payload.notif = "false";
-    payload.ia = "false";
   }
 }
 
